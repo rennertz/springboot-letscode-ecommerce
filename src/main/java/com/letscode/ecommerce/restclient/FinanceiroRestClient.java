c0e201929e2fc5459340aa36e9c38232dff70036
F8583484 Renner Tetzner Ramos <renner.tetzner@bb.com.br>
2022-09-02 18:16:48 -0300

    produtos 2

---
+++ src/main/java/com/letscode/ecommerce/restclient/FinanceiroRestClient.java
@@ -0,0 +1,18 @@
+package com.letscode.ecommerce.restclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@FeignClient(
+        value = "financeiroRestClient",
+        url = "https://dummyjson.com")
+public interface FinanceiroRestClient {
+
+    @RequestMapping(path="/products/categories", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
+    List<String> findAll();
+}
